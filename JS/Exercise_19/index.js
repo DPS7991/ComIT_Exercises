ec2ef34322e0c6f903a9c9622d33253d5e90bf1e
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-09-21 20:08:08 -0500

    Exercise 19 complete!

---
+++ JS/Exercise_19/index.js
@@ -0,0 +1,20 @@
+const birthMonth = 'October';
+const firstName = 'Daniel';
+const lastName = 'Peter-Salawu'
+
+let city;
+let age;
+let nextCity;
+
+city = 'Winnipeg';
+age = 100;
+nextCity = city;
+
+console.log('month of birth:', birthMonth);
+console.log('first name:', firstName);
+console.log('last name:', lastName);
+console.log('city:', city);
+console.log('age:', age);
+console.log('next city:', nextCity);
+
+console.log(`${firstName} ${lastName}`);
