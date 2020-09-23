c6ec2e3b70cdfc4d30da5059ccb46b846923b4b2
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-09-23 17:49:50 -0500

    Exercise 20 done!

---
+++ JS/Exercise_20/index.js
@@ -0,0 +1,21 @@
+let age = 100;
+let firstName = 'Daniel';
+let courseCompleted = false;
+let finalScore = null;
+
+let winningNumbers;
+
+console.log('age:', age);
+console.log('first name:', firstName);
+console.log('course completed:', courseCompleted);
+console.log('final score:', finalScore);
+
+let newAge = String(age);
+let newFirstName = Boolean(firstName);
+let newCourseCompleted = Number(courseCompleted);
+let newFinalScore = String(finalScore);
+
+console.log(newAge);
+console.log(newFirstName);
+console.log(newCourseCompleted);
+console.log(newFinalScore);
