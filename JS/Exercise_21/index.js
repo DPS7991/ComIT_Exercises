32e42b00b5b35240c32b3432bec587401c8db973
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-09-23 20:27:04 -0500

    Done!

---
+++ JS/Exercise_21/index.js
@@ -0,0 +1,6 @@
+alert('Enter two numbers to see if the first is greater than the second');
+
+let firstNumber = prompt('Enter the first number');
+let secondNumber = prompt('Enter the second number');
+
+confirm(firstNumber == secondNumber);
