42e50f4dbb69c612f69c86f692423bb398c62b72
Daniel Peter-salawu <66211631+DPS7991@users.noreply.github.com>
2020-09-24 10:48:38 -0500

    Update index.js

--- JS/Exercise_21/index.js
+++ JS/Exercise_21/index.js
@@ -3,4 +3,4 @@ alert('Enter two numbers to see if the first is greater than the second');
 let firstNumber = prompt('Enter the first number');
 let secondNumber = prompt('Enter the second number');
 
-confirm(firstNumber == secondNumber);
+confirm(firstNumber > secondNumber);
