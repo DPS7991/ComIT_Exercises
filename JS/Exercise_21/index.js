20482b2d4e426aae98f9aba13d29b8b1aa8c3760
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-09-24 11:08:55 -0500

    Done!

--- JS/Exercise_21/index.js
+++
@@ -1,6 +0,0 @@
-alert('Enter two numbers to see if the first is greater than the second');
-
-let firstNumber = prompt('Enter the first number');
-let secondNumber = prompt('Enter the second number');
-
-confirm(firstNumber == secondNumber);
---
+++ JS/Exercise_22/index.js
@@ -0,0 +1,15 @@
+alert('Enter two numbers to see if the first is greater than the second');
+let firstNumber = prompt('Enter the first number');
+let secondNumber = prompt('Enter the second number');
+
+let message;
+
+if (firstNumber > secondNumber) {
+    message = 'First number is greater';
+} else if (firstNumber < secondNumber) {
+    message = 'First number is lesser';
+} else if (firstNumber == secondNumber) {
+    message = 'Both numbers are the same!';
+}
+
+alert(message);
