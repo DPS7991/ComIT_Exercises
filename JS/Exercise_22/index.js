e4e89c8e208c1616bf7e10c462b5f9dd68ed8c12
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-09-25 09:20:54 -0500

    Changed the variable types to numbers

--- JS/Exercise_22/index.js
+++ JS/Exercise_22/index.js
@@ -1,6 +1,6 @@
 alert('Enter two numbers to see if the first is greater than the second');
-let firstNumber = prompt('Enter the first number');
-let secondNumber = prompt('Enter the second number');
+let firstNumber = Number(prompt('Enter the first number'));
+let secondNumber = Number(prompt('Enter the second number'));
 
 let message;
 
