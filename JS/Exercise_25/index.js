71b113f41926c6344a16f19ac85bca2f1e8d9f62
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-10-02 17:18:20 -0500

    Defined the message variable before switch statement

--- JS/Exercise_25/index.js
+++ JS/Exercise_25/index.js
@@ -16,37 +16,39 @@ if (weekdayNumber == 1) {
     console.log('Today is Sunday');
 }
 
+let message;
+
 switch (weekdayNumber) {
     case 1:
-        let message = 'Today is Monday';
+        message = 'Today is Monday';
         break;
 
     case 2:
-        let message = 'Today is Tuesday';
+        message = 'Today is Tuesday';
         break;
 
     case 3:
-        let message = 'Today is Wednesday';
+        message = 'Today is Wednesday';
         break;
         
     case 4:
-        let message = 'Today is Thursday';
+        message = 'Today is Thursday';
         break;
 
     case 5:
-        let message = 'Today is Friday';
+        message = 'Today is Friday';
         break;
 
     case 6:
-        let message = 'Today is Saturday';
+        message = 'Today is Saturday';
         break;
 
     case 7:
-        let message = 'Today is Sunday';
+        message = 'Today is Sunday';
         break;
     default:
-        let message = 'Number not between 1-7';
+        message = 'Number not between 1-7';
         break;
 }
 
-console.log('message');
+console.log(message);
