fc0cdc1436d94e1c66af836e6de943aac1b64feb
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-10-23 12:36:54 -0500

    Did the extra exercises except recreating map func

--- JS/Exercise_29/index.js
+++ JS/Exercise_29/index.js
@@ -26,3 +26,49 @@ for (let index = 0; index < newArr.length; index++) {
 for (let name of newArr) {
     console.log(name);
 }
+
+// Extra Exercises
+
+let favoriteRecipe = {
+    title: 'Mole',
+    servings: 2,
+    ingredients: ['cinnamon', 'cumin', 'cocoa']
+};
+
+console.log(favoriteRecipe['title']);
+console.log('Serves:', favoriteRecipe['servings']);
+console.log('Ingredients:');
+
+for (const item of favoriteRecipe['ingredients']) {
+    console.log(item);
+}
+
+let readingList = [
+    {
+        title: 'The Bible',
+        author: 'Many writers',
+        alreadyRead: true
+    },
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        alreadyRead: false
+    },
+    {
+        title: 'The Art of Meditation',
+        author: 'Joel GoldSmith',
+        alreadyRead: true
+    }
+]
+
+for (const book of readingList) {
+    console.log(`${book['title']} by ${book['author']}`);
+}
+
+for (const book of readingList) {
+    if (book['alreadyRead']) {
+        console.log(`You already read ${book['title']} by ${book['author']}`);
+    } else {
+        console.log(`You still need to read ${book['title']} by ${book['author']}`);
+    }
+}
