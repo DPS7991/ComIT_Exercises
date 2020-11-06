3daed47b77e6755b577ffe4f591e44299a028ae3
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2020-11-06 16:12:33 -0600

    Finished JSX exercise

---
+++ React/exercise-1/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color: 'red'}}>Daniel Peter-Salawu</h1>
+      <h5 style={{color: 'orange'}}>Learn React!</h5>
+    </div>
+  );
+}
+
+export default App;
